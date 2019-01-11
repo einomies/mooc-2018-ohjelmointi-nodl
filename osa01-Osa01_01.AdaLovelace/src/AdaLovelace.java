2ffebc5a135027157593fb03f82f766e35ba1b24
einomies26 <einokamarainen@hotmail.com>
2019-01-11 18:42:56 +0200

    eka

---
+++ osa01-Osa01_01.AdaLovelace/src/AdaLovelace.java
@@ -0,0 +1,11 @@
+
+import java.time.Clock;
+
+public class AdaLovelace {
+    
+    public static void main(String[] args) {
+        // Kirjoita ohjelmasi tähän alle
+        System.out.println("Ada Lovelace2");
+    }
+
+}
